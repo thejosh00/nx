012ccf3edc75b61c49da13487b9ed4d749fe439d
Joshua Hill <jhill@sonatype.com>
2022-09-19 10:27:10 -0500

    run task using type

--- task/run.go
+++ task/run.go
@@ -1,25 +1,50 @@
 package task
 
 import (
+	"encoding/json"
 	"fmt"
 	"org/sonatype/nx/api"
 )
 
 type TaskRunCommand struct {
+	UseType    bool `short:"t" long:"useType" description:"use type instead of id to locate task"`
 	Positional struct {
 		Value string `positional-arg-name:"value"`
 	} `positional-args:"yes" required:"true"`
 }
 
-func (cmd *TaskRunCommand) Execute(args []string) error {
-	taskId := cmd.Positional.Value
+type TaskItem struct {
+	Id string `json:"id"`
+}
 
-	err := runTask(taskId)
+type Tasks struct {
+	Items []TaskItem `json:"items"`
+}
+
+func (cmd *TaskRunCommand) Execute(args []string) error {
+	id := cmd.Positional.Value
+
+	if cmd.UseType {
+		resp, err := listTasks(cmd.Positional.Value)
+		if err != nil {
+			return err
+		}
+
+		var tasks Tasks
+		err = json.Unmarshal([]byte(resp), &tasks)
+		if err != nil {
+			return err
+		}
+
+		id = tasks.Items[0].Id
+	}
+
+	err := runTask(id)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("Ran task", taskId)
+	fmt.Println("Ran task", cmd.Positional.Value)
 	return nil
 }
 
