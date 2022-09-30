6a67bd2ea0246b0843242aefb585ebee72ddf047
Joshua Hill <jhill@sonatype.com>
2022-09-29 22:16:07 -0500

    add timeout to network-wait

--- network/network_wait.go
+++ network/network_wait.go
@@ -5,12 +5,14 @@ import (
 	"log"
 	"net"
 	"org/sonatype/nx/config"
+	"os"
 	"time"
 )
 
 type NetworkWaitCommand struct {
-	Host string `short:"h" long:"host" description:"host to connect to"`
-	Port string `short:"p" long:"port" description:"port to connect to"`
+	Host    string `short:"h" long:"host" description:"host to connect to"`
+	Port    string `short:"p" long:"port" description:"port to connect to"`
+	Timeout int    `short:"t" long:"timeout" description:"seconds to wait before failing"`
 }
 
 func (cmd *NetworkWaitCommand) Execute(args []string) error {
@@ -26,17 +28,23 @@ func (cmd *NetworkWaitCommand) Execute(args []string) error {
 		port = cmd.Port
 	}
 
-	wait(host, port)
+	wait(host, port, cmd.Timeout)
 	return nil
 }
 
-func wait(host string, port string) {
+func wait(host string, port string, timeout int) {
 	fmt.Println("Waiting for Nexus Repository to come online")
+	start := time.Now().Unix()
 	for {
 		log.Println("Connecting to ", host, ":", port)
 		conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), time.Second)
 		if err == nil && conn != nil {
 			break
+		}
+		timeLength := time.Now().Unix() - start
+		if timeout != 0 && timeLength > int64(timeout) {
+			fmt.Println("Timeout after", timeout, "seconds")
+			os.Exit(-1)
 		}
 		time.Sleep(time.Second)
 	}
